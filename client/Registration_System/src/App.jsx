31b780925034c6594357498f3e60e44046f48688
Ignas0315 <ignaszakaitis@gmail.com>
2023-08-05 23:04:59 +0300

    FINAL-0015: Adding Login page

--- client/Registration_System/src/App.jsx
+++ client/Registration_System/src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css';
 import { useContext } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes';
 
-const App = () => {
-    return <></>;
-};
+// const App = () => {
+//     return <></>;
+// };
+
+const App = () => <RouterProvider router={routes} />;
 
 export default App;
--- client/Registration_System/src/api/login-api.js
+++ client/Registration_System/src/api/login-api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 const baseURL = 'http://localhost:8080';
 
-export const login = (body) => axios.post(`${baseURL}/login`, body);
+export const login = (body) =>
+    axios.post(`${baseURL}/login`, body, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 
 /*
 post('/login
