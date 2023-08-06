6ebce7c0b81abf335994b5a0d25edad9c3ac3cea
Ignas0315 <ignaszakaitis@gmail.com>
2023-08-06 16:19:07 +0300

    FINAL-0019: Added login error message

--- client/Registration_System/src/api/login-api.js
+++ client/Registration_System/src/api/login-api.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 const baseURL = 'http://localhost:8080';
 
-export const login = (body) =>
-    axios.post(`${baseURL}/login`, body, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+export const login = async (body) => {
+    try {
+        return await axios.post(`${baseURL}/login`, body, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (err) {
+        return 'error';
+    }
+};
 
 /*
 post('/login
